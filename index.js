9d17b73ec0dc5ec2e59580b6defae9888172cc53
kajeen khalid <khalidkajeen@gmail.com>
2022-10-10 19:37:16 -0500

    commit tree 93f56acbcfa0e27418f3ba3e144ca4147d797d45

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  let result
+
+  if (distance > 2500) {
+    return (result = 'No can do.')
+  } else if (distance > 2000) {
+    return (result = 'I will gladly take your thirty bucks.')
+  } else if (distance > 400 && distance <= 1500) {
+    return 'That will be twenty bucks.'
+  } else if (distance <= 400) {
+    return (result = 'This one is on me!')
+  }
+}
+console.log(scuberGreetingForFeet(199))
+console.log(scuberGreetingForFeet(1500))
+console.log(scuberGreetingForFeet(2001))
+console.log(scuberGreetingForFeet(2501))
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
+} 
